7667a895164396d4da3835863802463a9472a58f
SupremePrimeRick <alfredfredrick41@gmail.com>
2023-02-22 04:09:35 -0600

    api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
-
-'''
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
-'''
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-def call():
-    """ function that carry out the activity"""
-    Id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users" + "/" + Id
-
-    response = requests.get(url)
-    Name = response.json().get('username')
-
-    todo = url + "/todos"
-    tasks = requests.get(todo).json()
-
-    dictionary = {Id: []}
-    for task in tasks:
-        new_dic = {"task": task.get('title'),
-                   "completed": task.get('completed'),
-                   "username": Name}
-        dictionary[Id].append(new_dic)
-
-    with open('{}.json'.format(Id), 'w') as file:
-        json.dump(dictionary, file)
-
-
-if __name__ == '__main__':
-    call()
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
